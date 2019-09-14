a3a60db356dc01b88dc3526b083b0551f9870e1a
ScarletGuo <zguo74@wisc.edu>
2019-09-14 11:05:59 -0500

    socket

--- lib739kv.cpp
+++ lib739kv.cpp
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include <string.h>
 #define NUM_SERVER 3;
+#define BUFFER_SIZE 1024;
 
+char* port;
+char* host;
 int sockets[NUM_SERVER];
 struct sockaddr_in server_addrs[NUM_SERVER];
 char buffer[1024] = {0};
@@ -12,6 +15,12 @@ char buffer[1024] = {0};
 int kv739_init(char ** server_list)
 {
 	// go through server list
+	size_t i;
+	// init all servers with name host:post
+	while (server_list[i] != NULL) {
+		// extract host & port
+		char* server_name = strtok(server_list[i], ":");			
+	}
 	return 0;
 }
 
@@ -46,29 +55,31 @@ int init_single_server(int server_id, char* host, char* port)
 
 int kv739_put(char * key, char * value, char * old_value)
 {
+
+/*
 	char msg[strlen(key) + strlen(value) + strlen(old_value) + 2];
 	strcpy(msg, key);
 	strcat(msg, ",");
 	strcat(msg, value);
 	strcat(msg, ",");
 	strcat(msg, old_value);
+*/
+	
 	send_msg(0, msg);
 	return 0;
 }
 
 int kv739_get(char * key, char * value)
 {
-	char msg[strlen(key) + strlen(value) + 1];
-	strcpy(msg, key);
-	strcat(msg, ",");
-	strcat(msg, value);	
-	send_msg(0, msg);
+	string url = "http//\{" + host + "\}/kv/?k=\{" + key + "\}";
+	char* valread[BUFFER_SIZE];
+	send_msg(0, msg, valread);
 	return 0;
 }
 
-int send_msg(int server_id, char* msg)
+void send_msg(int server_id, char* msg, char* valread)
 {
 	send(sockets[server_id], msg, strlen(msg));
-	valread = read(sockets[server_id], buffer, 1024);
-	return 0;
+	strcpy(valraed, read(sockets[server_id], buffer, 1024));
+	
 }
