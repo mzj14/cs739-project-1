fd15ea34a70e0971957f898ae94d481266225720
mzj14 <mazijun2017@outlook.com>
2019-09-21 13:49:27 -0500

    update with leveldb

--- simple_http_server.py
+++ simple_http_server.py
@@ -3,8 +3,7 @@ from urllib.parse import urlparse
 import json
 import sys
 
-# from pymongo import MongoClient
-# from pprint import pprint
+import plyvel
 
 class RequestHandler(BaseHTTPRequestHandler):
     protocol_version = "HTTP/1.1"
@@ -12,11 +11,11 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         if parsed_path.path == "/kv/":
-            print('key is = ', parsed_path.query.split("=")[-1])
-            message = json.dumps({
-                "success": "yes",
-                "value": "777"
-            })
+            # print('key is = ', parsed_path.query.split("=")[-1])
+            k = parsed_path.query.split("=")[-1]
+            v = db.get(k.encode())
+            s = {"is_key_in": "yes", "value": v.decode()} if v else {"is_key_in": "no", "value": "None"}
+            message = json.dumps(s)
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header("Content-Length", len(message))
@@ -30,12 +29,13 @@ class RequestHandler(BaseHTTPRequestHandler):
         data = json.loads(post_body.decode())
         parsed_path = urlparse(self.path)
         if parsed_path.path == "/kv/":
-            print("post key is = ", data['k'])
-            print("post value is = ", data['v'])
-            message = json.dumps({
-                "success": "yes",
-                "old_value": "666"
-            })
+            # print("post key is = ", data['k'])
+            # print("post value is = ", data['v'])
+            k, v = data['k'], data['v']
+            old_v = db.get(k.encode())
+            db.put(k.encode(), v.encode())
+            s = {"is_key_in": "yes", "old_value": old_v.decode()} if old_v else {"is_key_in": "no", "old_value": "None"}
+            message = json.dumps(s)
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header("Content-Length", len(message))
@@ -44,12 +44,9 @@ class RequestHandler(BaseHTTPRequestHandler):
         return
 
 if __name__ == '__main__':
-    # client = MongoClient("mongodb://localhost:27017/")
-    # db = client.admin
-    # serverStatusResult = db.command("serverStatus")
-    # pprint(serverStatusResult)
-
     system_ip, system_port = sys.argv[1], int(sys.argv[2])
+    # reconnect to the database
+    db = plyvel.DB('/tmp/cs739db-%d/' % system_port, create_if_missing=True)
     server = HTTPServer((system_ip, system_port), RequestHandler)
     print('Starting server at http://%s:%d' % (system_ip, system_port))
     server.serve_forever()
