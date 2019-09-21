adea21234c8a5f65e7f43f5e0205828593557f06
ScarletGuo <zguo74@wisc.edu>
2019-09-21 08:37:55 -0500

    completed client connection

--- lib739kv.h
+++ lib739kv.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <memory>
 #include <regex>
+#include <vector>
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 #include <cpprest/containerstream.h>
@@ -24,26 +25,63 @@ int kv739_init(char ** server_list)
 	// init all servers with name host:post
 	while (server_list[i] != NULL) {
 		// extract host & port
-                //http_client client(U("http://www.bing.com/"));
-		client_ptr = new http_client(U("http://google.com"));
-		//lib739kv_client = http_client(web::uri(utility::conversions::to_string_t()));
+		vector<char*> v;
+		char* chars_array = strtok(server_list[i], ":");
+		while(chars_array)
+    		{
+        		v.push_back(chars_array);
+        		chars_array = strtok(NULL, ":");
+    		}	
+		//client_ptr = new http_client(U("http://google.com"));
+		uri_builder builder(U("http://"));
+		builder.set_host(v[0]);
+		builder.set_port(atoi(v[1]));
+		client_ptr = new http_client(builder.to_string());
+		cout << "Establish Connection to: " << builder.to_string() << endl;
 		break;
 		i++;		
 	}
 	return 0;
 }
 
-/*
+
 int kv739_put(char * key, char * value, char * old_value)
 {
+	
 	concurrency::streams::stringstreambuf buffer;
 	// build request
 	uri_builder builder(U("/kv/"));
 	web::json::value json_post;
+	web::json::value json_return;
 	json_post["k"] = web::json::value::string(key);
 	json_post["v"] = web::json::value::string(value);
 	// send request
-    client.request(methods::POST, builder.to_string(), json_post)
+	client_ptr->request(methods::POST, builder.to_string(), json_post)
+		.then([](const web::http::http_response& response) {
+        		return response.extract_json(); 
+    		})
+    		.then([&json_return](const pplx::task<web::json::value>& task) {
+        	try {
+            		json_return = task.get();
+        	}
+        	catch (const web::http::http_exception& e) {                    
+            		std::cout << "error " << e.what() << std::endl;
+        	}
+    		})
+    		.wait();
+	// TODO: parse json to get value
+    	std::cout << json_return.serialize() << std::endl;
+	return 0;
+}
+
+int kv739_get(char * key, char * value)
+{
+	concurrency::streams::stringstreambuf buffer;
+	web::json::value json_return;
+	// build request
+	uri_builder builder(U("/kv/?k="));
+    builder.append_query(U(key));
+    client_ptr->request(methods::GET, builder.to_string())    
     .then([](const web::http::http_response& response) {
         return response.extract_json(); 
     })
@@ -61,27 +99,3 @@ int kv739_put(char * key, char * value, char * old_value)
 	return 0;
 }
 
-int kv739_get(char * key, char * value)
-{
-	concurrency::streams::stringstreambuf buffer;
-	// build request
-	uri_builder builder(U("/kv/?k="));
-    builder.append_query(U(key));
-    client.request(methods::GET, builder.to_string())    
-    .then([](const web::http::http_response& response) {
-        return response.extract_json(); 
-    })
-    .then([&json_return](const pplx::task<web::json::value>& task) {
-        try {
-            json_return = task.get();
-        }
-        catch (const web::http::http_exception& e) {                    
-            std::cout << "error " << e.what() << std::endl;
-        }
-    })
-    .wait();
-    // TODO: parse json to get value
-    std::cout << json_return.serialize() << std::endl;
-	return 0;
-}
-*/
