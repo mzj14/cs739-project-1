53ad400819f34a19fc3a704fa3c30f22eba0374c
ScarletGuo <zguo74@wisc.edu>
2019-09-21 00:43:23 -0500

    resolved global reference

--- Makefile
+++ Makefile
@@ -3,18 +3,19 @@
 
 CXX = g++
 CXXFLAGS = -O2 -g -Wall -std=c++11 
-OBJECTS = main.o lib739kv.o
+CXXLINK = -lcpprest -lboost_system -lcrypto -lssl 
+OBJECTS = main.cpp
 
 # During debugging you may want to used the compiler flags listed below
 #CXXFLAGS =      -g -Wall
 
-all: main.o lib739kv.o
-	$(CXX) $(CXXFLAGS) $(OBJECTS) -o output
+all: main.cpp
+	$(CXX) $(CXXFLAGS) $(OBJECTS) -o output $(CXXLINK)
 
 main.o: main.cpp
 	$(CXX) $(CXXFLAGS) -c main.cpp
 
-lib739kv.o: lib739kv.cpp lib739kv.h
+lib739kv.o: lib739kv.h 
 	$(CXX) $(CXXFLAGS) -c lib739kv.cpp
 
 clean:
--- lib739kv.h
+++ lib739kv.h
@@ -1,5 +1,3 @@
-#ifndef LIB739KV_H
-#define LIB739KV_H
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,10 +8,80 @@
 #include <cpprest/filestream.h>
 #include <cpprest/containerstream.h>
 
-#include <cpprest/http_client.h>
+using namespace std;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
 
-int kv739_init(char ** server_list);
 
-extern web::http::client::http_client lib739kv_client;
+web::http::client::http_client* client_ptr;
 
-#endif // LIB739KV_H
+
+int kv739_init(char ** server_list)
+{
+	// go through server list
+	size_t i = 0;
+	// init all servers with name host:post
+	while (server_list[i] != NULL) {
+		// extract host & port
+                //http_client client(U("http://www.bing.com/"));
+		client_ptr = new http_client(U("http://google.com"));
+		//lib739kv_client = http_client(web::uri(utility::conversions::to_string_t()));
+		break;
+		i++;		
+	}
+	return 0;
+}
+
+/*
+int kv739_put(char * key, char * value, char * old_value)
+{
+	concurrency::streams::stringstreambuf buffer;
+	// build request
+	uri_builder builder(U("/kv/"));
+	web::json::value json_post;
+	json_post["k"] = web::json::value::string(key);
+	json_post["v"] = web::json::value::string(value);
+	// send request
+    client.request(methods::POST, builder.to_string(), json_post)
+    .then([](const web::http::http_response& response) {
+        return response.extract_json(); 
+    })
+    .then([&json_return](const pplx::task<web::json::value>& task) {
+        try {
+            json_return = task.get();
+        }
+        catch (const web::http::http_exception& e) {                    
+            std::cout << "error " << e.what() << std::endl;
+        }
+    })
+    .wait();
+    // TODO: parse json to get value
+    std::cout << json_return.serialize() << std::endl;
+	return 0;
+}
+
+int kv739_get(char * key, char * value)
+{
+	concurrency::streams::stringstreambuf buffer;
+	// build request
+	uri_builder builder(U("/kv/?k="));
+    builder.append_query(U(key));
+    client.request(methods::GET, builder.to_string())    
+    .then([](const web::http::http_response& response) {
+        return response.extract_json(); 
+    })
+    .then([&json_return](const pplx::task<web::json::value>& task) {
+        try {
+            json_return = task.get();
+        }
+        catch (const web::http::http_exception& e) {                    
+            std::cout << "error " << e.what() << std::endl;
+        }
+    })
+    .wait();
+    // TODO: parse json to get value
+    std::cout << json_return.serialize() << std::endl;
+	return 0;
+}
+*/
