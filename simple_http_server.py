f548aea5a6dab11d8f3db78f2f82babcb3e20121
ScarletGuo <zguo74@wisc.edu>
2019-09-27 22:56:37 -0500

    untested server

--- simple_http_server.py
+++ simple_http_server.py
@@ -4,7 +4,8 @@ import json
 import sys
 import datetime
 
-import plyvel
+#import plyvel
+import sqlite3
 import requests
 import threading
 
@@ -29,7 +30,19 @@ class RequestHandler(BaseHTTPRequestHandler):
             # print('key is = ', parsed_path.query.split("=")[-1])
             print("receive read request")
             k = parsed_path.query.split("=")[-1]
-            v = db1.get(k.encode())
+            # v = db1.get(k.encode())
+
+            # get value for key = k
+            c = conn.cursor()
+            # grab old value
+            c.execute('''SELECT VALUE FROM KVSTORE WHERE KEY = %s''' % k)
+            return_val = c.fetchone()
+            if return_val:
+                v = return_val[0]
+            else:
+                v = None
+            conn.commit()
+
             s = {"is_key_in": "yes", "value": v.decode()} if v else {"is_key_in": "no", "value": "None"}
             message = json.dumps(s)
             self.send_response(200)
@@ -38,13 +51,25 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(message.encode())
             return
+
         # data recover from failure
         if parsed_path.path == "/rec/":
             print("receive recover request")
             t = parsed_path.query.split("=")[-1]
+
+            # grab all the k, v pairs with timestamp >= t
+            c = conn.cursor()
+            # grab old value
+            c.execute('''SELECT KEY, VALUE FROM KVSTORE WHERE TIMESTAMP >= %s''' % t)
+            kv_pairs = c.fetchall()
+            conn.commit()
+
             dic = {}
-            for k, v in db2.iterator(start=t.encode()):
-                dic[k.decode()] = v.decode()
+
+            if kv_pairs:
+                for k, v in kv_pairs:
+                    dic[k] = v
+
             message = json.dumps(dic)
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -65,18 +90,33 @@ class RequestHandler(BaseHTTPRequestHandler):
             print("receive write request")
             t = str(datetime.datetime.now())
             k, v = data['k'], data['v']
-            old_t = db3.get(k.encode())
-            if old_t:
-                # delete old timestamp
-                db2.delete((old_t.decode() + "[" + k).encode())
-            # add new timestamp with this key
-            db2.put((t + "[" + k).encode(), v.encode())
-            # update timestamp
-            db3.put(k.encode(), t.encode())
-            # record key-value
-            old_v = db1.get(k.encode())
-            # update value
-            db1.put(k.encode(), v.encode())
+
+            # old_t = db3.get(k.encode())
+            # if old_t:
+            #     # delete old timestamp
+            #     db2.delete((old_t.decode() + "[" + k).encode())
+            # # add new timestamp with this key
+            # db2.put((t + "[" + k).encode(), v.encode())
+            # # update timestamp
+            # db3.put(k.encode(), t.encode())
+            # # record key-value
+            # old_v = db1.get(k.encode())
+            # # update value
+            # db1.put(k.encode(), v.encode())
+
+            # put v into k if old_t < t
+            c = conn.cursor()
+            # grab old value
+            c.execute(''' SELECT VALUE FROM KVSTORE INDEXED BY idx_key WHERE KEY = %s; ''' % k)
+            return_val = c.fetchone()
+            if return_val:
+                old_v = return_val[0]
+                c.execute('''UPDATE KVSTORE SET TIMESTAMP = %s, VALUE = %s WHERE KEY = %s AND TIMESTAMP <= %s;''' % (t, v, k, t))
+            else:
+                old_v = None
+                c.execute('''INSERT INTO KVSTORE VALUES (%s, %s, %s);''' % (k, v, t))
+            conn.commit()
+
             # launch http request to sync data for other servers
             # even if a server crashes, we will still try to sync with it
             for port in server_ports:
@@ -102,16 +142,31 @@ class RequestHandler(BaseHTTPRequestHandler):
             # print("post timestamp is = ", data['t'])
             print("receive sync request")
             k, v, t = data['k'], data['v'], data['t']
-            old_t = db3.get(k.encode())
-            if old_t:
-                # delete old timestamp
-                db2.delete((old_t.decode() + "[" + k).encode())
-            # add new timestamp with this key
-            db2.put((t + "[" + k).encode(), v.encode())
-            # update timestamp
-            db3.put(k.encode(), t.encode())
-            # update value
-            db1.put(k.encode(), v.encode())
+
+            # old_t = db3.get(k.encode())
+            # if old_t:
+            #     # delete old timestamp
+            #     db2.delete((old_t.decode() + "[" + k).encode())
+            # # add new timestamp with this key
+            # db2.put((t + "[" + k).encode(), v.encode())
+            # # update timestamp
+            # db3.put(k.encode(), t.encode())
+            # # update value
+            # db1.put(k.encode(), v.encode())
+
+            # put v into k if old_t < t
+            c = conn.cursor()
+            # grab old value
+            c.execute(''' SELECT VALUE FROM KVSTORE INDEXED BY idx_key WHERE KEY = %s; ''' % k)
+            return_val = c.fetchone()
+            if return_val:
+                old_v = return_val[0]
+                c.execute('''UPDATE KVSTORE SET TIMESTAMP = %s, VALUE = %s WHERE KEY = %s AND TIMESTAMP <= %s;''' % (t, v, k, t))
+            else:
+                old_v = None
+                c.execute('''INSERT INTO KVSTORE VALUES (%s, %s, %s);''' % (k, v, t))
+            conn.commit()
+
             message = json.dumps({})
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -140,19 +195,34 @@ def recover_db():
                     for tk, v in r.json().items():
                         t, k = tk.split("[")[:2]
                         print(t, k, v)
-                        old_t = db3.get(k.encode())
-                        if old_t and old_t.decode() < t:
-                            continue
-                        else:
-                            if old_t:
-                                # delete old timestamp
-                                db2.delete((old_t.decode() + "[" + k).encode())
-                            # add new timestamp
-                            db2.put((t + "[" + k).encode(), v.encode())
-                            # update timestamp
-                            db3.put(k.encode(), t.encode())
-                            # update value
-                            db1.put(k.encode(), v.encode())
+
+
+                        # old_t = db3.get(k.encode())
+                        # if old_t and old_t.decode() < t:
+                        #     continue
+                        # else:
+                        #     if old_t:
+                        #         # delete old timestamp
+                        #         db2.delete((old_t.decode() + "[" + k).encode())
+                        #     # add new timestamp
+                        #     db2.put((t + "[" + k).encode(), v.encode())
+                        #     # update timestamp
+                        #     db3.put(k.encode(), t.encode())
+                        #     # update value
+                        #     db1.put(k.encode(), v.encode())
+
+
+                    # put v into k if old_t < t
+                    c = conn.cursor()
+                    # grab old value
+                    c.execute(''' SELECT VALUE FROM KVSTORE INDEXED BY idx_key WHERE KEY = %s; ''' % k)
+                    return_val = c.fetchone()
+                    if return_val:
+                        c.execute('''UPDATE KVSTORE SET TIMESTAMP = %s, VALUE = %s WHERE KEY = %s AND TIMESTAMP <= %s;''' % (t, v, k, t))
+                    else:
+                        c.execute('''INSERT INTO KVSTORE VALUES (%s, %s, %s);''' % (k, v, t))
+                    conn.commit()
+
                     # done with the recovery
                     break
                 else:
@@ -164,13 +234,24 @@ def recover_db():
 if __name__ == '__main__':
     server_ip, server_ports, server_index = sys.argv[1], sys.argv[2].split(','), int(sys.argv[3])
     server_port = server_ports[server_index]
+
     # reconnect to the database
-    # key->value
-    db1 = plyvel.DB('/tmp/cs739db-%s-1/' % server_port, create_if_missing=True)
-    # timestampe+key->value
-    db2 = plyvel.DB('/tmp/cs739db-%s-2/' % server_port, create_if_missing=True)
-    # key->timestamp
-    db3 = plyvel.DB('/tmp/cs739db-%s-3/' % server_port, create_if_missing=True)
+    # # key->value
+    # db1 = plyvel.DB('/tmp/cs739db-%s-1/' % server_port, create_if_missing=True)
+    # # timestampe+key->value
+    # db2 = plyvel.DB('/tmp/cs739db-%s-2/' % server_port, create_if_missing=True)
+    # # key->timestamp
+    # db3 = plyvel.DB('/tmp/cs739db-%s-3/' % server_port, create_if_missing=True)
+
+    conn = sqlite3.connect('/tmp/cs739db-%s-1/' % server_port)
+    cursor = conn.cursor()
+    # create table with schema (key, value, timestamp)
+    cursor.execute('''CREATE TABLE KVSTORE ( KEY TEXT PRIMARY KEY NOT NULL, VALUE TEXT NOT NULL, TIMESTAMP BIGINT NOT NULL);''')
+    # create indexes for time and key
+    cursor.execute('''CREATE INDEX idx_timestamp ON KVSTORE(TIMESTAMP);''')
+    cursor.execute('''CREATE INDEX idx_key ON KVSTORE(KEY);''')
+    conn.commit()
+
     # set recover flag
     rec = False
     # lauch a thread for data restore
