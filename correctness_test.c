393ca4b7e2279387e59137bda65482819c7335af
mzj14 <mazijun2017@outlook.com>
2019-10-01 13:10:11 -0500

    update correctness_test.c

--- correctness_test.c
+++ correctness_test.c
@@ -7,7 +7,14 @@
 
 #include "739kv.h"
 
+#ifdef DATA_CONFLICT
+#define TEST_NUM 2000
+#define VERSION_NUM 5
+#else
 #define TEST_NUM 10000
+#define VERSION_NUM 1
+#endif
+
 #define KEY_SIZE 20
 #define VALUE_SIZE 20
 
@@ -21,28 +28,31 @@ int main(int argc, char * argv[]) {
     pid_t process_id = getpid();
 
     /* init server */
-    kv739_init(argv + 1);
+    kv739_init(argv + 3);
 
     /* initialized work loads */
     /* initialize random seed */
     srand(time(NULL));
-    char key[TEST_NUM][KEY_SIZE] = {0};
-    char value_1[TEST_NUM][VALUE_SIZE] = {0};
+    char key[TEST_NUM * VERSION_NUM][KEY_SIZE] = {0};
+    char value_1[TEST_NUM * VERSION_NUM][VALUE_SIZE] = {0};
     char value_2[TEST_NUM][VALUE_SIZE] = {0};
-    for (int i = 0; i < TEST_NUM; i++) {
-        sprintf(key[i], "%d-%d", rand(), process_id);
-        sprintf(value_1[i], "%x-%x", rand(), process_id);
+    for (int i = 0; i < TEST_NUM * VERSION_NUM; i++) {
+        sprintf(key[i], "%d-%d", process_id, i % TEST_NUM);
+        sprintf(value_1[i], "%x-%x", process_id, i / TEST_NUM);
     }
 
-    printf("%d write requests will be issued in %d seconds,\
-        please be ready to shutdown/restart server...\n", TEST_NUM, SLEEP_INTERVAL);
+    printf("%d write requests will be issued in %d seconds, please be ready to shutdown/restart server...\n", TEST_NUM, SLEEP_INTERVAL);
+    sleep(SLEEP_INTERVAL);
+    printf("begin write request...\n");
 
-    for (int i = 0; i < TEST_NUM; i++) {
-        kv739_put(key[i], value_1[i], value_2[i]);
+    /* write request */
+    for (int i = 0; i < TEST_NUM * VERSION_NUM; i++) {
+        kv739_put(key[i], value_1[i], value_2[i % TEST_NUM]);
     }
 
-    printf("%d read requests will be issued in %d seconds,\
-        please be ready to shutdown/restart server...\n", TEST_NUM, SLEEP_INTERVAL);
+    printf("%d read requests will be issued in %d seconds, please be ready to shutdown/restart server...\n", TEST_NUM, SLEEP_INTERVAL);
+    sleep(SLEEP_INTERVAL);
+    printf("begin read reqeust\n");
 
     /* read requests */
     for (int i = 0; i < TEST_NUM; i++) {
@@ -51,12 +61,13 @@ int main(int argc, char * argv[]) {
 
     /* measure correctness */
     int correct = 0;
+    int delta = TEST_NUM * VERSION_NUM - TEST_NUM;
     for (int i = 0; i < TEST_NUM; i++) {
-        if (strcmp(value_1[i], value_2[i]) == 0) {
+        if (strcmp(value_1[i + delta], value_2[i]) == 0) {
             correct += 1;
         } else {
             printf("Test Error: In case %d, we should get value %s not %s\n",
-                i, value_1[i], value_2[i]);
+                i, value_1[i + delta], value_2[i]);
         }
     }
 
