9457e8175dc1754445d76c9b2d5cd16dae2d3506
ScarletGuo <zguo74@wisc.edu>
2019-10-01 13:55:50 -0500

    add scripts

---
+++ test_performance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#ip=$1
+ip="128.105.14.94"
+echo ${ip}
+n_server=$1
+mkdir ptest/${n_server}
+for i in $(seq 1 $n_server)
+do
+	echo ${n_server}-$i
+	#echo "./performance_test ${ip}:6666 ${ip}:6667 ${ip}:6668 ${ip}:6669 &>> ptest/${n_server}/$i.log &"
+	./performance_test ${ip}:6666 ${ip}:6667 ${ip}:6668 ${ip}:6669 &>> ptest/${n_server}/$i.log &
+done
