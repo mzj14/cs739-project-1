75635b05eb92db07e4dbb30f4e8c549ca957b117
ScarletGuo <zguo74@wisc.edu>
2019-10-01 10:50:04 -0500

    backup on git

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "lib739kv.so"
-
-int main(int argc,char *argv[]) {
-    char** server_list;
-    server_list[0] = argv[1];
-    std::cout << kv739_init(server_list) << std::endl;
-    return 0;
-}
